a9c06dfa120c72225f8f25336408ebe6b59d0e64
djoni1vincent <75507349+djoni1vincent@users.noreply.github.com>
2025-09-17 14:07:41 +0200

    represent snake as an array of coordinates.

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "red";
 ctx.fillRect(0,0,300,300);
+
+let snake = [ {x: 150, y: 150}, {x: 140, y: 150}, {x: 130, y: 150}, {x: 120, y: 150}, {x: 110, y: 150} ]
