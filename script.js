db1b1c4243c5ce14f07ca4095459ccacf3930633
djoni1vincent <75507349+djoni1vincent@users.noreply.github.com>
2025-09-18 11:12:22 +0200

    add function to control a snake

--- script.js
+++ script.js
@@ -24,6 +24,29 @@ function drawSnakePart(snakePart) {
     ctx.strokeRect(snakePart.x, snakePart.y, 10, 10); 
 }
 
+function changeDirection(event) {
+    const LEFT_KEY = 37; 
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+
+    const keyPressed = event.keyCode;
+    const goingUp = dy === 10;
+    const goingDown = dy === -10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;    
+
+    if (keyPressed === LEFT_KEY && !goingRight) {
+        dx = -10; dy = 0;
+    } else if(keyPressed === UP_KEY && !goingDown ){
+       dx = 0; dy = -10; 
+    } else if (keyPressed === RIGHT_KEY && !goingLeft) {
+        dx = 10; dy = 0 
+    } else if (keyPressed === DOWN_KEY && !goingUp) {
+        dx = 0; dy = 10;
+    }
+}
+
 function advanceSnake () {
     const head = {x: snake[0].x + dx, y: snake[0].y + dy};
     snake.unshift(head);
@@ -45,4 +68,7 @@ function main() {
     advanceSnake();  
     drawSnake();main();}, 100);
 }
+
+document.addEventListener("keydown", changeDirection)
+
 main();
